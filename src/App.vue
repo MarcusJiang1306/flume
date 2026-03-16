43a12584fe1b0e47699aab1bcbb8654e19c9fbba
Marcus Jiang <jiang950526@qq.com>
2026-03-17 00:39:26 +0800

    feat: 更新固定版本策略，修改默认css行为

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,4 @@ import FlumeProvider from './components/FlumeProvider.vue';
 </template>
 
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  font-family: Arial, sans-serif;
-  overflow: hidden;
-  width: 100vw;
-  height: 100vh;
-}
-
-#app {
-  width: 100vw;
-  height: 100vh;
-}
 </style>
